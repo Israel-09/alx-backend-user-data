ff025bf13a8d6e1bf82cadea5fe253b745f61d2e
israel-09 <israelinene91@gmail.com>
2024-04-17 14:56:35 +0100

    fixed auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -49,8 +49,8 @@ def before_request():
     if auth:
         excluded_paths = ['/api/v1/status/',
                           '/api/v1/unauthorized/', '/api/v1/forbidden/']
-        auth.require_auth(request.path, excluded_paths)
-
+        if auth.require_auth(request.path, excluded_paths) == False:
+            return
         if auth.authorization_header(request) is None:
             abort(401)
         if auth.current_user(request) is None:
