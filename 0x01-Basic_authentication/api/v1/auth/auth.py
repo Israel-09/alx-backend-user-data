7276918eac56448ea4173aa1169c66d4956390dd
israel-09 <israelinene91@gmail.com>
2024-04-17 13:26:20 +0100

    tasj 4 solution

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,8 +8,14 @@ from typing import List, TypeVar
 class Auth:
     """Autication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """require auth"""
-        return False
+        """check if session requires auth"""
+        if not path or (not excluded_paths or len(excluded_paths) == 0):
+            return True
+
+        path = path.rstrip('/') + '/'
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header"""
