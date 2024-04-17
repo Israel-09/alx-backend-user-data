a3424177d8f0adac60257e490c9efe7cadae9571
israel-09 <israelinene91@gmail.com>
2024-04-17 15:22:58 +0100

    task 7 solution

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,11 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """basic auth template"""
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extract base 64 from the header"""
+        if type(authorization_header) is str:
+            if authorization_header.startswith('Basic '):
+                return authorization_header.split()[1]
+        return None
