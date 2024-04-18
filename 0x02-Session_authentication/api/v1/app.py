dcc05bab368aeb47bdd4b1be8c71003e986d59f1
israel-09 <israelinene91@gmail.com>
2024-04-18 14:39:07 +0100

    task 5 solution

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,9 +54,12 @@ def before_request():
     """check if enpoint requires authentication"""
     if auth:
         excluded_paths = ['/api/v1/status/',
-                          '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                          '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                          '/api/v1/auth_session/login/']
+
         if auth.require_auth(request.path, excluded_paths) is True:
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401)
             current_user = auth.current_user(request)
             if current_user is None:
