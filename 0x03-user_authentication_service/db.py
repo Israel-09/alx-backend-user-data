c99ee3eadf28f9df9bd890d9c03a67ff2e2bd623
israel-09 <israelinene91@gmail.com>
2024-04-23 13:41:48 +0100

    task 1 solution

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,9 @@
-#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound
 
 from typing import TypeVar, Dict
 from user import Base, User
@@ -18,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,20 +31,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> TypeVar(User):
-        """add new user to the database"""
+        """adds a new user to the database"""
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
-
-    def find_user_by(self, **kwargs) -> User:
-        """find user by a parameter"""
-        session = self._session
-        user = session.query(User).filter_by(**kwargs).first()
-        if user is None:
-            raise NoResultFound()
-        return user
-
-    def commit(self):
-        """commit change"""
-        self._session.commit()
