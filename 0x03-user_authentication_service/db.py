1d578ee08ed008525819206271d4e104e88e27ca
israel-09 <israelinene91@gmail.com>
2024-04-23 15:14:22 +0100

    added find_user methed

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from typing import TypeVar, Dict
 from user import Base, User
@@ -38,3 +40,14 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """find user by a parameter"""
+        if not kwargs:
+            raise InvalidRequestError
+
+        session = self._session
+        user = session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user
