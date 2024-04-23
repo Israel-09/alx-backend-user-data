28d96d6d189831bebbaf2874f4dfeabe974578a4
israel-09 <israelinene91@gmail.com>
2024-04-23 15:09:32 +0100

    fixed annotation

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,8 +31,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar(User):
-        """adds a new user to the database"""
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """adds a new user to the database
+        """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
