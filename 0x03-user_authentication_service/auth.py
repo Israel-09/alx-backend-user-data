6439833732feea9bb5e426833ab85c71f89a32c4
israel-09 <israelinene91@gmail.com>
2024-04-25 09:33:46 +0100

    update auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,7 +67,7 @@ class Auth():
         """
         try:
             user = self._db.find_user_by(email=email)
-            self._db.update_user(user.id, session_id=_generate_uuid())
+            user.session_id = _generate_uuid())
             return user.session_id
         except NoResultFound:
             return None
